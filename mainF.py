7d7b98777613be35a1822b8aa80c9e508326bb88
Shyamsundar Naik <45869930+shyamsundar-naik@users.noreply.github.com>
2022-07-14 06:54:32 +0530

    Add files via upload

---
+++ mainF.py
@@ -0,0 +1,31 @@
+import Utils.listQ
+import Utils.tupleQ
+import Utils.strF
+import Utils.dictQ
+
+
+lQ = Utils.listQ.listQuestions()
+lQ.ex2Question1()
+lQ.ex2Question3()
+lQ.ex2Question4()
+lQ.ex2Question5()
+lQ.ex3Question5()
+tQ = Utils.tupleQ.tupleQuestions()
+tQ.ex3Question5()
+sQ = Utils.strF.strQuestions()
+sQ.ex1Question1()
+sQ.ex1Question2()
+sQ.ex1Question3()
+sQ.ex1Question4()
+sQ.ex1Question5()
+sQ.ex1Question7()
+sQ.ex1Question8()
+sQ.ex1Question9()
+sQ.ex3Question12()
+dQ = Utils.dictQ.dicQuestions()
+dQ.ex2Question6()
+dQ.ex2Question7()
+dQ.ex3Question4()
+dQ.ex3Question11()
+
+
